697a33a8ddd798b52805c063239927814eab790b
Ramesh Beharry <ramesh.beharry@mailfence.com>
2024-01-24 16:21:55 -0500

    added 404 error to rorschach_tests POST

--- api/routers/rorschach.py
+++ api/routers/rorschach.py
@@ -31,15 +31,20 @@ async def get_rorschach_image(
     return repo.get_all()
 
 
-@router.post("/api/rorschach_tests", response_model=RorschachTestOut)
+@router.post("/api/rorschach_tests", response_model=Union[RorschachTestOut, Error])
 # Include union of error handling. union: {some error model here}
 async def create_rorschach_test(
     info: RorschachTestIn,
-    # request: Request,
-    # response: Response,
+    request: Request,
+    response: Response,
     repo:  RorschachTestQueries = Depends()
 ):
-    return repo.create(info)
+    rorschach_test = repo.create(info)
+    if isinstance(rorschach_test, Error):
+        response.status_code = 404
+    else:
+        response.status_code = 200
+    return rorschach_test
 
 
 @router.put("/api/rorschach_tests/{rorschach_id}", response_model=Union[RorschachTestOut, Error])
