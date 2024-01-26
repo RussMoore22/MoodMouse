5b7df3ec58f2f6cef61ee024e9894b9ae1c2e396
Russ Moore <russcmoore314@gmail.com>
2024-01-25 20:01:20 -0500

    added pydantic typing to each query method and each router function excluding check-ins, which are tracked by another gitlab issue

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -11,6 +11,7 @@ from typing import Union
 
 router = APIRouter()
 
+
 @router.get("/token", response_model=AccountToken | None)
 async def get_token(
     request: Request,
@@ -23,13 +24,14 @@ async def get_token(
             "account": account,
         }
 
+
 @router.post("/api/accounts", response_model=Union[AccountToken, Error])
 async def create__account(
     info: AccountIn,
     request: Request,
     response: Response,
     repo: AccountsQueries = Depends()
-):
+) -> AccountToken | Error:
     hashed_password = authenticator.hash_password(info.password)
     account = repo.create(info=info, hashed_password=hashed_password)
     if isinstance(account, Error):
