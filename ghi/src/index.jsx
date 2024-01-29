5f776d7d64302a4274e29be36ddf48fc0ce96e04
Luna Sun <Luyisun0412@email.here>
2024-01-29 15:46:03 -0600

    updated changes to redux configuration

--- ghi/src/index.jsx
+++ ghi/src/index.jsx
@@ -5,9 +5,11 @@ import App from './App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 )
