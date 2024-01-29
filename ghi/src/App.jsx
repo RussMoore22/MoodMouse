666479e2e2b6ae9d250d4936e877f6b4f95e46c0
Luna Sun <Luyisun0412@email.here>
2024-01-29 17:02:14 -0600

    fixed error for react-redux

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 
     return (
         <BrowserRouter>
-            {/* <Nav /> */}
+            <Nav />
 
             <div>
                 <ErrorNotification error={error} />
