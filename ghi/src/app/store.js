1dc54d4cee8c4392c1c63776e1dc6b67c6fc1d62
Jennifer <jenniferperera12@gmail.com>
2024-01-29 15:21:58 -0500

    Added changes to store.js and apiSlice.jsx

--- ghi/src/app/store.js
+++ ghi/src/app/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+// import queryReducer from './querySlice'
+import { moodmouseApi } from './apiSlice'
 
 export const store = configureStore({
-  reducer: {},
+    reducer: {
+        // query: queryReducer,
+        [moodmouseApi.reducerPath]: moodmouseApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(moodmouseApi.middleware),
 })
 
+setupListeners(store.dispatch)
