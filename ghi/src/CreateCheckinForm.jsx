5f016455e2c77195b3668cb7a8e78d9965eb6926
Russ Moore <russcmoore314@gmail.com>
2024-01-31 19:51:15 -0500

    added input tags for questions.

--- ghi/src/CreateCheckinForm.jsx
+++ ghi/src/CreateCheckinForm.jsx
@@ -11,9 +11,9 @@ function CreateCheckinForm() {
     const [date, setDate] = useState('')
     const [updatedDate, setUpdatedDate] = useState('')
     const [happyLevel, setHappyLevel] = useState('')
-    const [survey, setSurvey] = useState('')
+    const [survey, setSurvey] = useState(0)
     const [journalEntry, setJournalEntry] = useState('')
-    const [rorschachTest, setRorschachTest] = useState('')
+    const [rorschachTest, setRorschachTest] = useState(0)
 
     const [q1, setQ1] = useState('')
     const [q1Ans, setQ1Ans] = useState('')
@@ -35,27 +35,33 @@ function CreateCheckinForm() {
     const [rorschachImg, setRorschachImage] = useState({})
     const [questions, setQuestions] = useState([])
 
-    const [createCheckin] = useCreateCheckinMutation()
-    const [createSurvey] = useCreateSurveyMutation()
-    const [createRorschachTest] = useCreateRorschachTestMutation()
+    const [createCheckin, checkinStatus] = useCreateCheckinMutation()
+    const [createSurvey, surveyStatus] = useCreateSurveyMutation()
+    const [createRorschachTest, rorschachStatus] = useCreateRorschachTestMutation()
     const { data: rorschach_imgs, isLoading: r_isLoading } = useGetImagesQuery()
+    let nowJSON = new Date(Date.now()).toJSON()
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log('Submit button clicked')
-        createRorschachTest({ image, response })
-    }
+
     const handleHappyLevel = (event) => {
         setHappyLevel(event.target.value)
-    }
-    const handleSurveyChange = (event) => {
-        setSurvey(event.target.value)
     }
     const handleJournalEntry = (event) => {
         setJournalEntry(event.target.value)
     }
-    const handleQ1Ans = (event) => {
+    const handleQ1 = (event) => {
         setQ1Ans(event.target.value)
+    }
+    const handleQ2 = (event) => {
+        setQ2Ans(event.target.value)
+    }
+    const handleQ3 = (event) => {
+        setQ3Ans(event.target.value)
+    }
+    const handleQ4 = (event) => {
+        setQ4Ans(event.target.value)
+    }
+    const handleQ5 = (event) => {
+        setQ5Ans(event.target.value)
     }
 
     function getRandomRorschachImg() {
@@ -74,6 +80,56 @@ function CreateCheckinForm() {
     const { data: question4, isLoading: q4_isLoading } = useGetQuestionQuery(4)
     const { data: question5, isLoading: q5_isLoading } = useGetQuestionQuery(5)
 
+    const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Submit button clicked')
+    const image_id = 1
+    createRorschachTest({ image_id, response })
+    const q1q = +question1.id
+    const q2q = +question2.id
+    const q3q = +question3.id
+    const q4q = +question4.id
+    const q5q = +question5.id
+    createSurvey({
+        q1q,
+        q1Ans,
+        q2q,
+        q2Ans,
+        q3q,
+        q3Ans,
+        q4q,
+        q4Ans,
+        q5q,
+        q5Ans,
+         })
+
+        }
+
+    if (!surveyStatus.isLoading && !rorschachStatus.isLoading && checkinStatus.isUninitialized){
+        if (surveyStatus.isSuccess && rorschachStatus.isSuccess){
+            // const now = ((new Date(Date.now())).toJSON()
+            const e = (new Date(Date.now())).toISOString()
+            setDate("1975-08-19T23:15:30.000Z")
+            setUpdatedDate("1975-08-19T23:15:30.000Z")
+            console.log("testing the survey:", surveyStatus)
+            console.log("testing the rorschach:", rorschachStatus)
+            setRorschachTest(+rorschachStatus.data.id)
+            setSurvey(+surveyStatus.data.survey_id)
+            createCheckin({
+                    date,
+                    updatedDate,
+                    happyLevel,
+                    journalEntry,
+                    survey,
+                    rorschachTest
+            })
+
+    if (checkinStatus.isError){
+        console.log(checkinStatus)
+        }
+        }
+    }
+
     function getQuestions() {
         // if (
         //     !(
@@ -89,7 +145,7 @@ function CreateCheckinForm() {
         // }
         const arr = []
         if (!q1_isLoading) {
-            arr.push(question1)
+            arr.push({question: question1, ans: q1Ans})
             if (!q2_isLoading) {
                 arr.push(question2)
                 if (!q3_isLoading) {
@@ -104,6 +160,7 @@ function CreateCheckinForm() {
             }
         }
         setQuestions(arr)
+
     }
 
     // questions state contains 5 question objects
@@ -151,9 +208,9 @@ function CreateCheckinForm() {
                     </p>
                 )}
             </div>
-            <div>
-                <p>{questions.map((question) => question.prompt)}</p>
-            </div>
+            {/* <div>
+                {questions.map((question) => <p>{question.prompt}</p>)}
+            </div> */}
 
             <div className="row">
                 <form id="user-checkin-form" onSubmit={handleSubmit}>
@@ -162,7 +219,7 @@ function CreateCheckinForm() {
                     </div>
                     <div>
                         <div className="form-group col-md-6">
-                            <label htmlFor="happyLevel"></label>
+                            <label htmlFor="happyLevel">Happy Level </label>
                             <input
                                 type="number"
                                 className="form-control"
@@ -170,19 +227,32 @@ function CreateCheckinForm() {
                                 placeholder="0"
                                 onChange={handleHappyLevel}
                                 value={happyLevel}
+                                min="0"
+                                max="4"
                             />
                         </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="q1Ans"></label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="q1Ans"
-                                placeholder="Enter your answer here"
-                                onChange={handleQ1Ans}
-                                value={q1Ans}
-                            />
+
+                        <div>
+                            <label for="question1">{ q1_isLoading ? 'loading...' : question1.prompt } </label>
+                            <input type="number" name="question1" id="question1" min="0" max="4" onChange={handleQ1} value={q1Ans} />
                         </div>
+                        <div>
+                            <label for="question2">{ q2_isLoading ? 'loading...' : question2.prompt } </label>
+                            <input type="number" name="question1" id="question2" min="0" max="4" onChange={handleQ2} value={q2Ans} />
+                        </div>
+                        <div>
+                            <label for="question3">{ q2_isLoading ? 'loading...' : question3.prompt } </label>
+                            <input type="number" name="question1" id="question3" min="0" max="4" onChange={handleQ3} value={q3Ans} />
+                        </div>
+                        <div>
+                            <label for="question4">{ q2_isLoading ? 'loading...' : question4.prompt } </label>
+                            <input type="number" name="question1" id="question4" min="0" max="4" onChange={handleQ4} value={q4Ans} />
+                        </div>
+                        <div>
+                            <label for="question5">{ q2_isLoading ? 'loading...' : question5.prompt } </label>
+                            <input type="number" name="question1" id="question5" min="0" max="4" onChange={handleQ5} value={q5Ans} />
+                        </div>
+
                         <div className="form-group col-md-6">
                             <label htmlFor="journalEntry"></label>
                             <input
