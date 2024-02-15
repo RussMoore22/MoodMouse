fd9ddd1e672d9e02c0b312e63a59959681193844
Ramesh Beharry <ramesh.beharry@mailfence.com>
2024-02-15 13:36:17 -0500

    changed a tag and home.js and git yml

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
             },
         ],
     },
-])
+], {basename: '/moodmouse'})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
